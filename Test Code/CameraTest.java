becea5b511c20e2a076e05a4f6d6d2e8be53018e
Mang00oo <134463083+Mang00oo@users.noreply.github.com>
2023-11-28 14:59:08 -0800

    Upload CameraTest.java

---
+++ Test Code/CameraTest.java
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.robotcore.external.android.util.Size;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp
+
+public class CameraTest extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() {
+
+        AprilTagProcessor tagProcessor = new AprilTagProcessor.Builder()
+            .setDrawAxes(true)
+            .setDrawCubeProjection(true)
+            .setDrawTagID(true)
+            .setDrawTagOutline(true)
+            .build();
+        
+        //Size size = new Size(640, 480);
+        VisionPortal visionPortal = new VisionPortal.Builder()
+            .addProcessor(tagProcessor)
+            .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+            .setCameraResolution(new android.util.Size(640, 480))
+            .build();
+        
+        waitForStart();
+        
+        while (!isStopRequested() && opModeIsActive()) {
+            
+            if (tagProcessor.getDetections().size() > 0){
+                AprilTagDetection tag = tagProcessor.getDetections().get(0);
+                telemetry.addData("x", tag.ftcPose.x);
+                telemetry.addData("y", tag.ftcPose.y);
+                telemetry.addData("z", tag.ftcPose.z);
+                telemetry.addData("pitch", tag.ftcPose.pitch);
+                telemetry.addData("yaw", tag.ftcPose.yaw);
+                telemetry.addData("roll", tag.ftcPose.roll);
+                
+                telemetry.update();
+            }
+        }
+        
+    }
+}
