9372df9bf5c00658857423bc40385685947aae01
Mang00oo <134463083+Mang00oo@users.noreply.github.com>
2024-02-01 14:06:31 -0800

    Update MainAuton.java
    Partially adds going to the board

--- Auton Code/MainAuton.java
+++ Auton Code/MainAuton.java
@@ -26,204 +26,261 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 @Autonomous(name="MainAuton", group="Main Code")
 
 public class MainAuton extends LinearOpMode {
-    private IMU imuAsIMU;
-    private double robotYaw;
-    private double leftPower;
-    private double rightPower;
-    private double targetRot;
-    private double tick;
-    private DcMotor backRight;
-    private DcMotor backLeft;
-    private DcMotor frontLeft;
-    private DcMotor frontRight;
-    private double distance_M;
-    private DistanceSensor distance;
-    private ColorSensor color;
-    private double hspeed;
-    private double vspeed;
-    private double codeStep;
+	private IMU imuAsIMU;
+	private double robotYaw;
+	private double leftPower;
+	private double rightPower;
+	private double targetRot;
+	private double tick;
+	private DcMotor backRight;
+	private DcMotor backLeft;
+	private DcMotor frontLeft;
+	private DcMotor frontRight;
+	private double distance_M;
+	private DistanceSensor distance;
+	private ColorSensor color;
+	private double hspeed;
+	private double vspeed;
+	private double codeStep;
+	private double propPos;
 
-    // Declare OpMode members.
-    private ElapsedTime runtime = new ElapsedTime();;
+	// Declare OpMode members.
+	private ElapsedTime runtime = new ElapsedTime();;
 
-    @Override
-    public void runOpMode() {
-        
-        // SET VARIABLES
-        YawPitchRollAngles orientation;
-        AngularVelocity angularVelocity;
-        robotYaw = 0;
-        leftPower = 0;
-        rightPower = 0;
-        targetRot = 0;
-    
-        hspeed = 0;
-        vspeed = 0;
-    
-        codeStep = 0;
-        
-        // Assign Motors
-        backRight = hardwareMap.get(DcMotor.class, "backRight");
-        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
-        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
-        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
-            
-        // Set Motor Directions
-        //frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        //backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
-            
-        // SETUP SENSORS
-        imuAsIMU = hardwareMap.get(IMU.class, "imu");
-        distance = hardwareMap.get(DistanceSensor.class, "DistanceSensor");
-        color = hardwareMap.get(ColorSensor.class, "colorSensor");
-            
-        //INITIALIZE IMU
-            imuAsIMU.initialize(new IMU.Parameters(new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.LEFT, RevHubOrientationOnRobot.UsbFacingDirection.UP)));
-        orientation = imuAsIMU.getRobotYawPitchRollAngles();
-        angularVelocity = imuAsIMU.getRobotAngularVelocity(AngleUnit.DEGREES);
-        robotYaw = orientation.getYaw(AngleUnit.DEGREES);
-        
-        telemetry.addData("IMU status", "Initialized");
-        telemetry.update();
-        
-        imuAsIMU.resetYaw();
-        
-        // Wait for the game to start (driver presses PLAY)
-        waitForStart();
-        runtime.reset();
+	@Override
+	public void runOpMode() {
+		
+		// SET VARIABLES
+		YawPitchRollAngles orientation;
+		AngularVelocity angularVelocity;
+		robotYaw = 0;
+		leftPower = 0;
+		rightPower = 0;
+		targetRot = 0;
+	
+		hspeed = 0;
+		vspeed = 0;
+	
+		codeStep = 0;
+		
+		// Assign Motors
+		backRight = hardwareMap.get(DcMotor.class, "backRight");
+		backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+		frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+		frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+			
+		// Set Motor Directions
+		//frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+		frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+		//backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+		backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+			
+		// SETUP SENSORS
+		imuAsIMU = hardwareMap.get(IMU.class, "imu");
+		distance = hardwareMap.get(DistanceSensor.class, "DistanceSensor");
+		color = hardwareMap.get(ColorSensor.class, "colorSensor");
+			
+		//INITIALIZE IMU
+			imuAsIMU.initialize(new IMU.Parameters(new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.LEFT, RevHubOrientationOnRobot.UsbFacingDirection.UP)));
+		orientation = imuAsIMU.getRobotYawPitchRollAngles();
+		angularVelocity = imuAsIMU.getRobotAngularVelocity(AngleUnit.DEGREES);
+		robotYaw = orientation.getYaw(AngleUnit.DEGREES);
+		
+		telemetry.addData("IMU status", "Initialized");
+		telemetry.update();
+		
+		imuAsIMU.resetYaw();
+		
+		// Wait for the game to start (driver presses PLAY)
+		waitForStart();
+		runtime.reset();
 
-        // Run until the end of the match (driver presses STOP)
-        while (opModeIsActive()) {
-            // Get the orientation and angular velocity.
-            orientation = imuAsIMU.getRobotYawPitchRollAngles();
-            angularVelocity = imuAsIMU.getRobotAngularVelocity(AngleUnit.DEGREES);
-            robotYaw = orientation.getYaw(AngleUnit.DEGREES);
-        
-            distance_M = distance.getDistance(DistanceUnit.CM);
-        
-            // Sets correct power for left and right sides of motors
-            if (robotYaw > targetRot){
-                leftPower = ((targetRot + robotYaw)/80)+vspeed;
-                rightPower = ((targetRot + robotYaw)/-80)+vspeed;
-            }
-            if (robotYaw < targetRot){
-                rightPower = ((targetRot + robotYaw)/-80)+vspeed;
-                leftPower = ((targetRot + robotYaw)/80)+vspeed;
-            }
-            
-            // Update Tick
-            tick = tick +1;
-            
-            // AUTON SEQUENCE
-            
-            // Step 1: Move Forward
-            if (codeStep == 0)
-            {
-                vspeed = 0.3;
-                if (tick > 40)
-                {
-                vspeed = 0;
-                tick = 0;
-                codeStep = 1;
-                }
-            }
-            
-            // Step 2: Check and Move
-            if (codeStep == 1)
-            {
-                // Check Middle Pos.
-                if (distance_M < 35)
-                {
-                    // Move forward
-                    vspeed = 0.3;
-                    if (tick > 20)
-                    {
-                        vspeed = 0;
-                        tick = 0;
-                        targetRot = 0;
-                        codeStep = 4;
-                    }
-                }
-                else
-                {
-                    tick = 0;
-                    codeStep = 2;
-                }
-            }    
-            
-            if (codeStep == 2)
-            {
-                // Check Right Pos.
-                //Turn
-                vspeed = 0;
-                targetRot = 50;
-                if(tick>20)
-                {
-                    //Check
-                    if (distance_M < 35)
-                    {
-                        //Move
-                        vspeed = 0.3;
-                        if (tick > 35)
-                        {
-                            // Stop Moving
-                            vspeed = 0;
-                            codeStep = 4;
-                        }
-                    }
-                    else if (tick < 25)
-                    {
-                        codeStep = 3;
-                        tick = 0;
-                    }
-                }
-            }
-            if (codeStep == 3)
-            {
-                // Go Left
-                // Turn
-                vspeed = 0;
-                targetRot = -60;
-                if(tick>20)
-                {
-                    // Move
-                    vspeed = 0.3;
-                    targetRot = -60;
-                    if (tick > 35)
-                    {
-                        // Stop Moving
-                        vspeed = 0.3;
-                        tick = 0;
-                        codeStep = 4;
-                    }
-                }
-            }
-            if (codeStep == 4)
-            {
-                vspeed = -0.3;
-                if (tick > 20)
-                {
-                    vspeed = 0;
-                    codeStep = 5;
-                }
-            }
-            
-            
-            // Set Motor Powers
-            frontLeft.setPower(-leftPower);
-            frontRight.setPower(-rightPower);
-            backLeft.setPower(-leftPower);
-            backRight.setPower(-rightPower);
-            
-            // Telemetry
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Distance", distance_M);
-            telemetry.addData("Tick", tick);
-            telemetry.addData("Yaw", robotYaw);
-            telemetry.addData("Code Step", codeStep);
-            telemetry.update();
-        }
-    }
+		// Run until the end of the match (driver presses STOP)
+		while (opModeIsActive()) {
+			// Get the orientation and angular velocity.
+			orientation = imuAsIMU.getRobotYawPitchRollAngles();
+			angularVelocity = imuAsIMU.getRobotAngularVelocity(AngleUnit.DEGREES);
+			robotYaw = orientation.getYaw(AngleUnit.DEGREES);
+		
+			distance_M = distance.getDistance(DistanceUnit.CM);
+		
+			// Sets correct power for left and right sides of motors
+			if (robotYaw > targetRot){
+				leftPower = ((targetRot + robotYaw)/80)+vspeed;
+				rightPower = ((targetRot + robotYaw)/-80)+vspeed;
+			}
+			if (robotYaw < targetRot){
+				rightPower = ((targetRot + robotYaw)/-80)+vspeed;
+				leftPower = ((targetRot + robotYaw)/80)+vspeed;
+			}
+			
+			// Update Tick
+			tick = tick +1;
+			
+			// AUTON SEQUENCE
+			
+			// Step 1: Move Forward
+			if (codeStep == 0)
+			{
+				vspeed = 0.3;
+				if (tick > 40)
+				{
+				vspeed = 0;
+				tick = 0;
+				codeStep = 1;
+				}
+			}
+			
+			// Step 2: Check and Move
+			if (codeStep == 1)
+			{
+				// Check Middle Pos.
+				if (distance_M < 45)
+				{
+					// Move forward
+					vspeed = 0.3;
+					if (tick > 20)
+					{
+						vspeed = 0;
+						tick = 0;
+						targetRot = 0;
+						propPos = 0;
+						codeStep = 4;
+					}
+				}
+				else
+				{
+					tick = 0;
+					codeStep = 2;
+				}
+			}	
+			
+			if (codeStep == 2)
+			{
+				// Check Right Pos.
+				//Turn
+				vspeed = 0;
+				targetRot = 50;
+				if(tick>20)
+				{
+					//Check
+					if (distance_M < 45)
+					{
+						//Move
+						vspeed = 0.3;
+						if (tick > 32)
+						{
+							// Stop Moving
+							vspeed = 0;
+							tick = 0;
+							propPos = 1;
+							codeStep = 4;
+						}
+					}
+					else if (tick < 25)
+					{
+						tick = 0;
+						propPos = 2;
+						codeStep = 3;
+					}
+				}
+			}
+			if (codeStep == 3)
+			{
+				// Go Left
+				// Turn
+				vspeed = 0;
+				targetRot = -60;
+				if(tick>20)
+				{
+					// Move
+					vspeed = 0.3;
+					targetRot = -60;
+					if (tick > 32)
+					{
+						// Stop Moving
+						vspeed = 0.3;
+						tick = 0;
+						codeStep = 4;
+					}
+				}
+			}
+			// Move back
+			if (codeStep == 4)
+			{
+				vspeed = -0.3;
+				if (tick > 15)
+				{
+					vspeed = 0;
+					tick = 0;
+					codeStep = 5;
+				}
+			}
+			// Gets ready to go to board
+			if (codeStep == 5)
+			{
+				targetRot = 0;
+				if(tick > 8)
+				{
+					vspeed = -0.3;
+				}
+				if (tick > 20)
+				{
+					vspeed = 0;
+					tick = 0;
+					targetRot = 90;
+					codeStep = 6;
+				}
+			}
+			// Goes to the board
+			if (codeStep == 6)
+			{
+				vspeed = 0.3;
+				if (distance_M < 19)
+				{
+					vspeed = 0;
+					tick = 0;
+					codeStep = 7;
+				}
+			}
+			// Goes to the right position
+			if (codeStep == 7)
+			{
+				if (propPos == 0)
+				{
+					hspeed = -0.2;
+					tick = 10;
+				}
+				if (propPos == 1)
+				{
+					tick = 21;
+				}
+				if (propPos == 2)
+				{
+					hspeed = -0.3;
+				}
+				if (tick > 20)
+				{
+					hspeed = 0;
+					tick = 0;
+					codeStep = 8;
+				}
+			}
+			
+			
+			// Set Motor Powers
+			frontLeft.setPower(-leftPower);
+			frontRight.setPower(-rightPower);
+			backLeft.setPower(-leftPower);
+			backRight.setPower(-rightPower);
+			
+			// Telemetry
+			telemetry.addData("Status", "Run Time: " + runtime.toString());
+			telemetry.addData("Distance", distance_M);
+			telemetry.addData("Tick", tick);
+			telemetry.addData("Yaw", robotYaw);
+			telemetry.addData("Code Step", codeStep);
+			telemetry.update();
+		}
+	}
 }
